6b6cfb37c572b9bf341ea47d412494768fa21156
jamos90 <jamos_98@hotmail.com>
2018-06-29 14:44:12 +0100

    hotel adding room working

--- src/main/java/Hotel/Hotel.java
+++ src/main/java/Hotel/Hotel.java
@@ -7,12 +7,12 @@ import Rooms.RoomType;
 import java.util.ArrayList;
 
 public class Hotel {
-    protected ArrayList<BedRoom> BedRooms;
+    protected ArrayList<BedRoom> bedRooms;
     protected ArrayList<ConferenceRoom> conferenceRooms;
 //    protected ArrayList<> dinningRooms;
 
     public Hotel(){
-        this.BedRooms = new ArrayList<>();
+        this.bedRooms = new ArrayList<>();
         this.conferenceRooms = new ArrayList<>();
 
     }
@@ -23,10 +23,23 @@ public class Hotel {
 //    }
 
     public int getBedroomCount() {
-        return this.BedRooms.size();
+        return this.bedRooms.size();
     }
 
     public int getConferenceRoomCount() {
         return this.conferenceRooms.size();
+    }
+
+    public void addRoom(BedRoom bedroom) {
+        this.bedRooms.add(bedroom);
+
+    }
+
+    public void addConferenceRoom(ConferenceRoom conferenceroom) {
+        this.conferenceRooms.add(conferenceroom);
+    }
+
+    public BedRoom getBedroom(BedRoom bedroom) {
+        return this.bedRooms.get(0);
     }
 }
--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -1,4 +1,6 @@
 import Hotel.Hotel;
+import Rooms.BedRoom;
+import Rooms.ConferenceRoom;
 import Rooms.RoomType;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,28 +10,57 @@ import static org.junit.Assert.assertEquals;
 public class HotelTest {
 
     Hotel hotel;
+    BedRoom bedroom;
+    BedRoom bedroom2;
+    BedRoom bedroom3;
+    ConferenceRoom conferenceroom;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         hotel = new Hotel();
+        bedroom = new BedRoom(1,50, 50, RoomType.DOUBLE_BEDROOM);
+        bedroom2 = new BedRoom(3,34,40,RoomType.DOUBLE_BEDROOM);
+        bedroom3 = new BedRoom(2,70,70,RoomType.DOUBLE_BEDROOM);
+        conferenceroom = new ConferenceRoom(50,"Confroom 1",200);
+
+
     }
 
     @Test
-    public void hotelHasNoBedRooms(){
+    public void hotelHasNoBedRooms() {
         assertEquals(0, hotel.getBedroomCount());
     }
 
     @Test
-    public void hotelHasNoConferenceRooms(){
+    public void hotelHasNoConferenceRooms() {
         assertEquals(0, hotel.getConferenceRoomCount());
     }
-//
+
+    @Test
+    public void canAddBedroomRoom() {
+        hotel.addRoom(bedroom);
+        assertEquals(1, hotel.getBedroomCount());
+    }
+
+    @Test
+    public void  canAddConferenceRoom(){
+        hotel.addConferenceRoom(conferenceroom);
+        assertEquals(1,hotel.getConferenceRoomCount());
+    }
+
 //    @Test
-//    public void hotelCanAddRooms(){
-//        hotel.addRoom(RoomType.SINGLE_BEDROOM);
-//        assertEquals(1, hotel.getSingleRoomCount());
+//    public void canGetCapacityFromArray(){
+//        hotel.addConferenceRoom(conferenceroom);
+//        assertEquals(50, .getCapacity());
 //    }
-//
-//    @Test
-//    public
+
+    @Test
+    public void canGetBedRoom(){
+        hotel.addRoom(bedroom);
+        hotel.addRoom(bedroom2);
+        hotel.addRoom(bedroom3);
+        assertEquals(bedroom, hotel.getBedroom(bedroom3));
+    }
+
 }
+
